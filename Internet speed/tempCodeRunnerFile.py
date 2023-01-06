87c2b67c048a48263661bffdfa774b487cdd146f
Varish Tyagi <114216483+varishtyagi351@users.noreply.github.com>
2023-01-06 16:21:18 +0530

    added

---
+++ Internet speed/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+lab = Label(sp,text="Download Speed", font=("Time New Roman", 30,
+#  "bold"),bg="Green", fg="Wheat")
+# lab.place(x=50,y=40)
+
+
+# lab = Label(sp,text="00", font=("Time New Roman", 30,
+#  "bold"),bg="Green", fg="Wheat")
+# lab.place(x=50,y=40)
+
+
+# lab = Label(sp,text="Upload Speed", font=("Time New Roman", 30,
+#  "bold"),bg="Green", fg="Wheat")
+# lab.place(x=50,y=40)
+
+
+# lab = Label(sp,text="00", font=("Time New Roman", 30,
+#  "bold"),bg="Green", fg="Wheat")
+# lab.place(x=50,y=40)
